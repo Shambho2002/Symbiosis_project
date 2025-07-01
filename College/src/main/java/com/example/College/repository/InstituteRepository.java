484e0f44b045c697f125fc79017bdd42d0bc3799
Shambho2002 <sharshambho@gmail.com>
2025-07-01 10:14:01 +0530

    Delete InstituteRepository.java

--- College/src/main/java/com/example/College/repository/InstituteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.College.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.College.model.Institute;
-
-public interface InstituteRepository extends JpaRepository<Institute,Long> {
-
-}
