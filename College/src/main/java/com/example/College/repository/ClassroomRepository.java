886f1a6df8b65ff7bc61400ca16a8994b69069ea
Shambho2002 <sharshambho@gmail.com>
2025-07-01 10:08:33 +0530

    Delete ClassroomRepository.java

--- College/src/main/java/com/example/College/repository/ClassroomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.College.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.College.model.Classroom;
-
-public interface ClassroomRepository extends JpaRepository<Classroom, Long>{
-
-}
