cb2557e0e6a0d9ae279a2164ceb66dfcda151cd8
Shambho2002 <sharshambho@gmail.com>
2025-07-01 10:14:11 +0530

    Delete AdminRepository.java

--- College/src/main/java/com/example/College/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.College.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.College.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin,Long> {
-
-}
